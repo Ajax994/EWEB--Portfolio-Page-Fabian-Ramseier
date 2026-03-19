ef236ddba645cd9b5ca6715648adc6e2d7a1a641
Ajax994 <fabian.ramseier@students.bfh.ch>
2026-03-19 09:37:05 +0100

    Added hero section with personal introduction and social links. Deleted the Test Button.

--- script.js
+++ script.js
@@ -13,24 +13,6 @@ navLinks.querySelectorAll('.nav-link').forEach(function(link) {
     });
 });
 
-const toggleBtn = document.getElementById('myButton1');
-const hiddenText = document.getElementById('hiddenText');
 
-// Event-Listener für den Button
-toggleBtn.addEventListener('click', function() {
-    // Alert anzeigen
-    alert('Hello World');
 
-    // Toggle-Klasse 'hidden' hinzufügen oder entfernen
-    hiddenText.classList.toggle('hidden');
-
-    // Button-Text und Status aktualisieren
-    if (hiddenText.classList.contains('hidden')) {
-        toggleBtn.textContent = 'Text anzeigen';
-        toggleBtn.classList.remove('active');
-    } else {
-        toggleBtn.textContent = 'Text verbergen';
-        toggleBtn.classList.add('active');
-    }
-});
 
